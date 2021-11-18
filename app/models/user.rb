edef2e784ade24d1c35279198977138a5c58aa02
Guilherme Amaral <40870111+gvma@users.noreply.github.com>
2021-11-18 11:13:32 -0300

    Revert "Feature/cors policy"

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  belongs_to :sector, optional: true
-end
--- db/migrate/20211115215758_add_sector_reference_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddSectorReferenceToUser < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :users, :sector, foreign_key: true
-  end
-end

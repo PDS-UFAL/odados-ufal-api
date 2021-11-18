d48b2d18e5bdf38efc651678986f155ce278ae4e
Thiago Tenório <thiago.tenorio@enjoei.com.br>
2021-11-18 11:06:00 -0300

    feat: add basic sectors validations

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,3 +1,9 @@
 class Sector < ApplicationRecord
   has_many :users
+
+  EMAIL_REGEX = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/.freeze
+
+  validates :email, uniqueness: true, presence: true, format: { with: EMAIL_REGEX, message: 'possui formato inválido' }
+  validates :name, presence: true
+  validates :responsible, presence: true
 end

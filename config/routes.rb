5e34fc622527be6244c6384a47b74e3c7e354215
Thiago Tenório <thiago.tenorio@enjoei.com.br>
2021-11-23 12:00:00 -0300

    feat(user): current_user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :forms
-  resources :users
+  resources :users do 
+    get '/me', to: 'users#me', on: :collection
+  end
   resources :sectors
   post '/login', to: 'auth#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

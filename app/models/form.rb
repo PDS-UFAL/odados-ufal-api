b4b10adcf1eabaf6fa1d5553f226f2f8b9c1e52f
Guilherme Amaral <gvma@ic.ufal.br>
2021-12-01 13:59:11 -0300

    feat: form and sector relationship

--- app/models/form.rb
+++ app/models/form.rb
@@ -11,6 +11,7 @@
 #  updated_at   :datetime         not null
 #
 class Form < ApplicationRecord
+	has_and_belongs_to_many :sectors
 	validates_presence_of :title, :initial_date, :end_date
 	validate :is_sections_an_array?
 

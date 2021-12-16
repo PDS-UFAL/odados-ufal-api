d03812b259089660662fab4b5704f55f44b55bc6
Thiago Tenório <ttcc@ic.ufal.br>
2021-12-15 22:18:30 -0300

    feat(form): add basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     get '/me', to: 'users#me', on: :collection
   end
   resources :sectors
+  resources :forms
   post '/login', to: 'auth#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

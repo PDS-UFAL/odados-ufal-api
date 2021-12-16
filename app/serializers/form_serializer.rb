4faa7459e46725b0df60d0433da738302beee6a9
Thiago Tenório <ttcc@ic.ufal.br>
2021-12-15 22:56:21 -0300

    feat(form): add sections, questions and responses

--- app/serializers/form_serializer.rb
+++ app/serializers/form_serializer.rb
@@ -11,6 +11,30 @@
 #  updated_at :datetime         not null
 #
 class FormSerializer < ActiveModel::Serializer
-  attributes :id, :title, :start_date, :end_date, :status, :created_at, :updated_at
-  has_many :sections
+  attributes :id, :title, :start_date, :end_date, :status, :created_at, :updated_at, :sections
+
+  def sections
+    object.sections&.map { |section| SectionSerializer.new(section) }
+  end
+
+  class SectionSerializer < ActiveModel::Serializer
+    attributes :id, :name, :questions
+
+    def questions
+      object.questions&.map { |question| QuestionSerializer.new(question) }
+    end
+
+    class QuestionSerializer < ActiveModel::Serializer
+      attributes :id, :title, :type, :required, :max_char, :max_value, :min_value, :options, :responses
+      
+      def responses
+        object.responses&.map { |response| ResponseSerializer.new(response) }
+      end
+
+      class ResponseSerializer < ActiveModel::Serializer
+        attributes :id, :answer
+        has_one :user
+      end
+    end
+  end
 end

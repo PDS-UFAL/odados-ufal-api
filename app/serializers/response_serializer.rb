8284c7db310a0a5adb17d276f3d1c8b3b2510590
Thiago Tenório <ttcc@ic.ufal.br>
2021-12-20 19:53:57 -0300

    run annotate

--- app/serializers/response_serializer.rb
+++ app/serializers/response_serializer.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: responses
+#
+#  id          :bigint           not null, primary key
+#  answer      :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  question_id :bigint           not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_responses_on_question_id  (question_id)
+#  index_responses_on_user_id      (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (question_id => questions.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class ResponseSerializer < ActiveModel::Serializer
   attributes :id, :answer
   has_one :user

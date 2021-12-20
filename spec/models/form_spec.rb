930bbcece1db8efd2de2f0ff0a8db04d6d7aa32a
Thiago Tenório <ttcc@ic.ufal.br>
2021-12-20 19:54:38 -0300

    feat(form): add factorie and model spec

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -12,6 +12,22 @@
 #
 require 'rails_helper'
 
-RSpec.describe Forms, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Form, type: :model, timecop: true do
+  describe 'associations' do
+    it { should have_many(:sections) }
+    it { should have_many(:form_sectors) }
+    it { should have_many(:sectors).through(:form_sectors) }
+    it { should accept_nested_attributes_for(:sections) }
+  end
+
+  describe 'enums' do
+    it { should define_enum_for(:status).with_values(closed: 0, open: 1) }
+  end
+
+  describe 'validations' do
+    [:title, :status, :start_date, :end_date].each { |attribute| it { should validate_presence_of(attribute) } }
+
+    it { should_not allow_value(Time.current.midnight - 1.second).for(:start_date) }
+    it { should_not allow_value(Time.current.midnight).for(:end_date) }
+  end
 end

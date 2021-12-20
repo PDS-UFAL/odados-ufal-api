605700dda0f712156169f5999e575ae96317e651
Thiago Tenório <ttcc@ic.ufal.br>
2021-12-20 19:54:19 -0300

    feat(user): add factorie and model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,71 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  role            :integer          default("employee"), not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  sector_id       :bigint
+#
+# Indexes
+#
+#  index_users_on_sector_id  (sector_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (sector_id => sectors.id)
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_secure_password }
+    it { should belong_to(:sector).optional }
+    it { should have_many(:responses) }
+  end
+
+  describe 'validations' do
+    subject { create(:user, :with_sector, sector_id: create(:sector).id ) }
+
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_length_of(:password).is_at_least(6) }
+    
+    [:email, :password_digest, :role].each { |attribute| it { should validate_presence_of(attribute) } }
+
+    context 'admin' do
+      before { allow(subject).to receive(:employee?).and_return(false) }
+
+      it { should_not validate_presence_of(:sector_id) }
+    end
+
+    context 'employee' do
+      before { allow(subject).to receive(:employee?).and_return(true) }
+
+      it { should validate_presence_of(:sector_id) }
+    end
+  end
+
+  describe 'enums' do
+    it { should define_enum_for(:role).with_values(admin: 0, employee: 1) }
+  end
+
+  describe 'scopes' do
+    let!(:sector) { create(:sector) }
+    let!(:user) { create(:user, :with_sector, sector_id: sector.id) }
+
+    context ':sector_id' do
+      it 'filters the users by sector_id, returns a json with only the users with the given sector_id' do              
+        expect(User.sector_id(1)).to all( have_attributes(sector_id: 1) )
+      end
+    end
+
+    context ':role' do
+      it 'filters the users by role, returns a json with only the users with the given role' do      
+        expect(User.role(1)).to all( have_attributes(role: 'employee') )
+      end
+    end
+  end
+end

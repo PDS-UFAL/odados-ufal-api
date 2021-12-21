95ffef0246b1ce87def9db1e3f4d7b3a34de2ddf
Thiago Tenório <ttcc@ic.ufal.br>
2021-12-21 18:26:41 -0300

    feat(form): add new enum and scopes tests

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Form, type: :model, timecop: true do
   end
 
   describe 'enums' do
-    it { should define_enum_for(:status).with_values(closed: 0, open: 1) }
+    it { should define_enum_for(:status).with_values(closed: 0, open: 1, not_started: 2) }
   end
 
   describe 'validations' do
@@ -29,5 +29,35 @@ RSpec.describe Form, type: :model, timecop: true do
 
     it { should_not allow_value(Time.current.midnight - 1.second).for(:start_date) }
     it { should_not allow_value(Time.current.midnight).for(:end_date) }
+  end
+
+  describe 'scopes' do
+    let!(:forms) { create_list(:form, 10) }
+    let(:form) { forms.first }
+
+    context ':title' do
+      it 'filters the forms by title, returns a json with only the forms with the given title' do
+        expect(Form.title(form.title)).to all( have_attributes(title: form.title) )
+      end
+    end
+
+    context ':status' do
+      it 'filters the forms by status, returns a json with only the forms with the given status' do
+        expect(Form.status(:open)).to all( have_attributes(status: 'open') )
+        expect(Form.status(:closed)).to all( have_attributes(status: 'closed') )
+      end
+    end
+
+    context ':start_date' do
+      it 'filters the forms by start_date, returns a json with only the forms with the given start_date' do
+        expect(Form.start_date(form.start_date)).to all( satisfy { |f| f.start_date >= Time.current.midnight } )
+      end
+    end
+
+    context ':end_date' do
+      it 'filters the forms by end_date, returns a json with only the forms with the given end_date' do
+        expect(Form.start_date(form.end_date)).to all( satisfy { |f| f.end_date >= Time.current.end_of_day } )
+      end
+    end
   end
 end
